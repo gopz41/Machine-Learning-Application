07f2aecb556cb0515f2154b995228e992c0e09da
Gopalakrishnan K <gopalakrishnan41@gmail.com>
2025-06-15 19:09:56 +1000

    comments added to setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
+#importing libraries
 from setuptools import find_packages, setup
 from typing import List
 
 REQUIREMENTS_FILE_NAME = "requirements.txt"
 HYPHEN_E_DOT = "-e ."
+#defining function to fetch the requirement.txt
+#takes input a filepath as string
+#gives output a list of strings
 
 
 def get_requirements()-> List [str]:
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
+
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
 
+#removing the (-e .) from the requirements.txt
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     
